337ce37e30d2e6293b50f827445d59457c3c78bd
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2018-10-17 10:38:48 +0900

    Letを解析できるようにする

---
+++ monkey/ast/ast.go
@@ -0,0 +1,49 @@
+package ast
+
+import (
+	"golang_projects/monkey/token"
+)
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token	// token.LET トークン
+	Name *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+// 識別子構造体
+type Identifier struct {
+	Token token.Token // token.IDENT トークン
+	Value string
+}
+
+func (ls *Identifier) statementNode() {}
+func (ls *Identifier) TokenLiteral() string { return ls.Token.Literal }
