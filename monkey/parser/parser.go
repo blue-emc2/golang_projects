ae14a57393ba772af47a0d5462d5d6c33b5bc4c5
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2018-10-17 14:35:03 +0900

    Errorsを追加

--- monkey/parser/parser.go
+++ monkey/parser/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"fmt"
 	"golang_projects/monkey/ast"
 	"golang_projects/monkey/token"
 	"golang_projects/monkey/lexer"
@@ -11,10 +12,15 @@ type Parser struct {
 
 	curToken token.Token	// 現在のトークン
 	peekToken token.Token	// 次のトークン
+
+	errors []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l: l,
+		errors: []string{},
+	}
 
 	// 2つトークンを読み込む。curTokenとpeekTokeの両方がセットされる。
 	p.nextToken()
@@ -85,6 +91,16 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
